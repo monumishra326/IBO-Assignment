a12b8ee14f9e5c9c930477f997a167699374d6cc
Monu MIshra <96140349+monumishra326@users.noreply.github.com>
2022-06-28 21:40:01 +0530

    unique key and remove duplicate and sum

---
+++ index.js
@@ -0,0 +1,147 @@
+/*
+You have been given a list of products which is having property productName, quantity and description.
+
+
+PROBLEM STATEMENTS:
+
+1. you need to write a function say, getUniqueProductCount which should return count of each Product(as an object) present in the given list of Products considering Product Name as Key.
+
+Sample Output for the given listOfProducts will be :
+
+{
+  "TV": 2,
+  "AC": 2,
+  "FAN": 1
+}
+
+
+
+2. you need to write a function say, getUniquePrducts which should return an array of objects by grouping the products based on the productName and summing up the quantity for the same products present in the given list of Products considering Product Name as Key.
+
+Sample Output for the given listOfProducts will be :
+
+[{
+    productName: "TV",
+    quantity: 20,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 10,
+    description: "air conditioner"
+  },
+  {
+    productName: "FAN",
+    quantity: 10,
+     description: "Ceiling Fan"
+  }
+]
+
+*/
+
+
+
+
+const listOfProducts = [{
+    productName: "TV",
+    quantity: 10,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner"
+  },
+  {
+    productName: "TV",
+    quantity: 10,
+    description: "television"
+  },
+  {
+    productName: "AC",
+    quantity: 5,
+    description: "air conditioner"
+  },
+  {
+    productName: "FAN",
+    quantity: 10,
+    description: "Ceiling Fan"
+  }
+];
+
+
+
+// 1. you need to write a function say, getUniqueProductCount which should return count of each Product(as an object) present in the given list of Products considering Product Name as Key.
+
+// Sample Output for the given listOfProducts will be :
+
+// {
+//   "TV": 2,
+//   "AC": 2,
+//   "FAN": 1
+// }
+
+
+
+/////////////////////////Solution//////////////////////
+
+var obj = {};
+
+
+for (var i in listOfProducts) {
+  var product = listOfProducts[i];
+  var productId =  product. productName;
+  var quantityId = product.quantity;
+
+	if (obj.hasOwnProperty(productId)) {
+  	obj[productId]++;
+  }
+  else {
+  	obj[productId] = 1;
+  }
+  
+
+}
+
+console.log(obj);
+
+
+
+
+
+
+
+// 2. you need to write a function say, getUniquePrducts which should return an array of objects by grouping the products based on the productName and summing up the quantity for the same products present in the given list of Products considering Product Name as Key.
+
+// Sample Output for the given listOfProducts will be :
+
+// [{
+//     productName: "TV",
+//     quantity: 20,
+//     description: "television"
+//   },
+//   {
+//     productName: "AC",
+//     quantity: 10,
+//     description: "air conditioner"
+//   },
+//   {
+//     productName: "FAN",
+//     quantity: 10,
+//      description: "Ceiling Fan"
+//   }
+// ]
+
+
+////////////////solution/////////////
+
+
+  
+  const result = listOfProducts.reduce((index, curr) => {
+    const objectad = index.find((o) => o.productName === curr.productName && o.description === curr.description);
+    if (objectad) objectad.quantity += curr.quantity;
+    else index.push(curr);
+    return index;
+  }, []);
+  
+  console.log(result);
